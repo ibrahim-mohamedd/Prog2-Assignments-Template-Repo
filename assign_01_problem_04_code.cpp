538f28586647318baf6288f03b5e4f5e8fb8c2aa
ibrahim-mohamedd <128511607+ibrahim-mohamedd@users.noreply.github.com>
2023-04-08 20:43:21 +0200

    Add files via upload

---
+++ assign_01_problem_04_code.cpp
@@ -0,0 +1,19 @@
+// ibrahim mohamed abbass  ID:20216490
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ int n;
+ long long z;
+ char f;
+ float x;
+  double y;
+  cin>> n >> z >> f >> x >> y ;
+  cout<< n <<"\n"<< z <<"\n"<< f <<"\n"<< x <<"\n"<< y <<"\n";
+   
+return 0;
+}
+
+
