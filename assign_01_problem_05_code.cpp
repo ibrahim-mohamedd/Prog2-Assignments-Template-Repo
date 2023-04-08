070027efeb8bd1b744dd66bdf143df5d70c30128
ibrahim-mohamedd <128511607+ibrahim-mohamedd@users.noreply.github.com>
2023-04-08 20:44:17 +0200

    Add files via upload

---
+++ assign_01_problem_05_code.cpp
@@ -0,0 +1,17 @@
+//ibrahim mohamed abbass ID:20216490
+#include <iostream>
+
+using namespace std;
+
+int main()
+{ float X,Y;
+cin>>X>>Y;
+    if (X>=1&&X<=1000000&&Y>=1&&Y<=1000000){
+        cout<<X<<" + "<<Y<<" = "<<X+Y<<endl;
+        cout<<X<<" * "<<Y<<" = "<<X*Y<<endl;
+        cout<<X<<" - "<<Y<<" = "<<X-Y<<endl;}
+        else;
+    return 0;
+}
+
+
