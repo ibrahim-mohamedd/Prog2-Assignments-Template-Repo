519d4d5a8c634cfe9a9a89805773b465141381b4
ibrahim-mohamedd <128511607+ibrahim-mohamedd@users.noreply.github.com>
2023-04-08 20:47:05 +0200

    Add files via upload

---
+++ assign_01_problem_07_code.cpp
@@ -0,0 +1,13 @@
+// ibrahim mohamed abbass  ID:20216490
+#include <iostream>
+
+using namespace std;
+
+int main()
+{ long long a,b,c,d,x;
+cin>>a>>b>>c>>d;
+    x = ( a * b ) - ( c * d );
+    cout<<"Difference = "<<x;
+    return 0;
+}
+
