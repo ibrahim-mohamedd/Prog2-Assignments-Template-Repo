625aa78ca842337ca32cf98089df1d2e22116c02
ibrahim-mohamedd <128511607+ibrahim-mohamedd@users.noreply.github.com>
2023-04-30 21:59:32 +0200

    Add files via upload

---
+++ assign_03_problem_03_code .java
@@ -0,0 +1,15 @@
+// ibrahim mohamed abbass    ID:20216490
+import java.util.Scanner;
+public class bebo {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+        int x= 1;
+        for (int i = 1; i <=number; i++) {
+            for(int y=1;y<=3;y++){
+            System.out.print(x + " ");
+            x++;
+        }
+        System.out.println("PUM");
+            x++;
+    }}}
