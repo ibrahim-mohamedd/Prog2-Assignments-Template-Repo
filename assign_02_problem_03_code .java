d8710a45a0dc6f2bfbe76b2e53dbaf9397e1e029
ibrahim-mohamedd <128511607+ibrahim-mohamedd@users.noreply.github.com>
2023-04-30 21:58:11 +0200

    Add files via upload

---
+++ assign_02_problem_03_code .java
@@ -0,0 +1,16 @@
+// ibrahim mohamed abbass    ID:20216490
+import java.util.Scanner;
+public class bebo
+{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+        for (int i = 1; i <= number; i++) {
+            for (int st = 1; st <= i; st++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+    }}
