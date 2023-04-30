a1297ec2cab8ad8aebc0f740c32304de40a5886b
ibrahim-mohamedd <128511607+ibrahim-mohamedd@users.noreply.github.com>
2023-04-30 21:58:47 +0200

    Add files via upload

---
+++ assign_02_problem_02_code.java
@@ -0,0 +1,16 @@
+//ibrahim mohamed abbass    ID:20216490
+
+import java.util.Scanner;
+public class bebo
+{
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        int n=input.nextInt();
+        for(int i=1;i<=12;i++){
+            int sum=i*n;
+            System.out.println(n+" * "+i+" = "+sum);
+
+            }
+        }
+    }
+
